0335be1d00a7a567e4c32743ce0a5511c69c77d2
shuai <shuaixie@zju.edu.cn>
2019-11-15 11:40:50 +0800

    update init.sh

--- hope/docker/init.sh
+++ hope/docker/init.sh
@@ -3,10 +3,9 @@
 sleep 5
 
 mysql -u root -e "CREATE DATABASE hope"
-mysql -u root -e "CREATE USER dataturks@'127.0.0.1' IDENTIFIED BY '12345';"
-mysql -u root -e "GRANT SELECT, INSERT, UPDATE, DELETE ON hope.* TO dataturks@127.0.0.1;FLUSH PRIVILEGES;"
+# 使其他 IP 也可访问数据库
+mysql -u root -e "CREATE USER dataturks@'%' IDENTIFIED BY '12345';"
+mysql -u root -e "GRANT SELECT, INSERT, UPDATE, DELETE ON hope.* TO dataturks@'%';FLUSH PRIVILEGES;"
+#mysql -u root -e "CREATE USER dataturks@'127.0.0.1' IDENTIFIED BY '12345';"
+#mysql -u root -e "GRANT SELECT, INSERT, UPDATE, DELETE ON hope.* TO dataturks@127.0.0.1;FLUSH PRIVILEGES;"
 mysql -u root hope < /home/dataturks/mysqlInit.sql
-
-# 创建新的 dataturks@% 用户，使其他 IP 也可访问数据库
-#CREATE USER dataturks@'%' IDENTIFIED BY '12345';
-#GRANT SELECT, INSERT, UPDATE, DELETE ON hope.* TO dataturks@'%';FLUSH PRIVILEGES;
